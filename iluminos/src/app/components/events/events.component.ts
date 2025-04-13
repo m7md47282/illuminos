70779223ff87e8924c27c60e076fe7bb8451f7b4
mohammad omari <m.omari@classera.com>
2025-04-14 01:25:58 +0300

    swiper

--- iluminos/src/app/components/events/events.component.ts
+++ iluminos/src/app/components/events/events.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, OnInit, Signal, viewChild, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-events',
@@ -11,10 +11,92 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class EventsComponent  implements OnInit {
-  images = [1, 2, 3,4,5,6].map((n) => `/assets/images/slider-${n}.png`);
+
+  swiperRef: Signal<ElementRef<any> | undefined> = viewChild('swiperRef')
+
+  events: {
+    id: number;
+    name: string;
+    description: string;
+    image: string;
+    date: string;
+    countdown?: any;
+  }[] = [
+    {
+      id: 1,
+      name: 'Event 1',
+      description: 'Event 1 Description',
+      image: '/assets/images/slider-1.png',
+      date: "2025-05-01" 
+    },
+    {
+      id: 2,
+      name: 'Event 2',
+      description: 'Event 2 Description',
+      image: '/assets/images/slider-2.png',
+      date: "2025-06-04"
+    },
+    {
+      id: 3,
+      name: 'Event 3',
+      description: 'Event 3 Description',
+      image: '/assets/images/slider-3.png',
+      date: "2025-05-10"
+    },
+    {
+      id: 4,
+      name: 'Event 4',
+      description: 'Event 4 Description',
+      image: '/assets/images/slider-4.png',
+      date: "2025-07-01"
+    },
+    {
+      id: 5,
+      name: 'Event 5',
+      description: 'Event 5 Description',
+      image: '/assets/images/slider-5.png',
+      date: "2025-07-20"
+    },
+    {
+      id: 6,
+      name: 'Event 6',
+      description: 'Event 6 Description',
+      image: '/assets/images/slider-6.png',
+      date: "2025-05-15"
+    }
+  ]
+
+  countdowns: any[] = [];
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.startCountdown();
+  }
 
+  ngAfterViewInit() {
+    if (this.swiperRef()) {
+      console.dir(this.swiperRef()?.nativeElement);
+    }
+  }
+  startCountdown() {
+    setInterval(() => {
+      this.events.forEach((event, index) => {
+        if (event.date) {
+          event.countdown = this.getTimeRemaining(new Date(event.date));
+        }
+      });
+    }, 1000);
+  }
+
+  getTimeRemaining(endtime: Date) {
+    const total = Date.parse(endtime.toString()) - Date.parse(new Date().toString());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+    return {
+      total, days, hours, minutes, seconds
+    };
+  }
 }
